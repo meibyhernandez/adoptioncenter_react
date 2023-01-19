6e40cb12dda4a72f21638cf96618f7b270f851c4
Meiby Hernández Morales <meahernandezmo@unal.edu.co>
2023-01-19 21:02:07 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,13 @@ const Navbar = () => (
         p={2}
         sx={{ position: 'sticky', background: '#F2F2F2', top: 0, justifyContent: 'space-between' }}>
             <img src={logo} alt="logo" height={100}></img>     
+            <ul className="app__navbar-links">
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#menu">Menu</a></li>
+        <li className="p__opensans"><a href="#awards">Awards</a></li>
+        <li className="p__opensans"><a href="#contact">Contact</a></li>
+      </ul>
     </Stack>
   
 )
